c3c1e6165a9ce68d2fbab0cd304cf1c11bd113ef
Jyotiprakash-Redapple <139202727+Jyotiprakash-Redapple@users.noreply.github.com>
2023-11-30 23:58:36 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
-"use client"
-import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main>
-      <div className='w-screen max-w-5xl min-h-screen align-middle' style={{ backgroundColor: 'white' }}>
-        <Image  height={1920} width={1200} src={'/bg.png'} alt='splash screen' />
-      </div>
-    </main>
-  )
+"use client";
+import Image from "next/image";
+
+export default function SplashScreen() {
+	return (
+		<main>
+			<div
+				className="w-screen h-screen md:flex items-center justify-center "
+				style={{ backgroundColor: "black" }}>
+				<div className="md:w-full md:h-full sm:w-full sm:h-full xs:w-full xs:h-full">
+					{/* <div></div> */}
+					<Image
+						src={"/bg.png"}
+						alt="Just Game"
+						width={1080}
+						height={1900}
+						priority={true}
+						className="h-full w-full object-contain"
+					/>
+					<Image
+						src={"/title_art.png"}
+						alt="Just Game"
+						width={611}
+						height={652}
+						priority={true}
+						className="w-2/12 object-contain"
+						style={{
+							position: "absolute",
+							top: 0,
+							right: 0,
+						}}
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
